e74711c7a1c1aa8b8fcdf79aed63ac5f3ddc339b
Empyrean-Void <jshortz@proton.me>
2023-02-15 10:51:43 -0700

    Initial commit

---
+++ calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+    // Create methods for basic math
+    static void add() {
+        
+    }
+
+    static void sub() {
+        
+    }
+
+    static void mult() {
+        
+    }
+
+    static void div() {
+        
+    }
+
+    // Create method to display menu
+    static void showMenu() {
+        System.out.println("(1) Add");
+        System.out.println("(2) Subtract");
+        System.out.println("(3) Multiply");
+        System.out.println("(4) Divide");
+        System.out.println("(5) Quit");
+    }
+
+    // Create methods to get input from user
+    public static void main(String[] args) {
+        showMenu();
+    }
+}
